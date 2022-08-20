64b795fd79c7011ae4332f685209e188daa52bf8
William Chalk <3halk1@gmail.com>
2022-08-20 18:37:58 -0400

    created inital quiz layout

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//When the start button is clicked then a timer starts and the first question appears
+//When a answer is selected then another question appers
+//When a question is answered incorrectly then time is subtracted from the clock
+//When all questions are answered or the time is equal to 0 then the quiz is over
+//When the game is over then I can enter my initials and score
